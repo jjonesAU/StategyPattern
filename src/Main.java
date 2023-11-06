e0e60a9056905874b4af3b149d25a325765fac32
jjonesAU <147194355+jjonesAU@users.noreply.github.com>
2023-11-06 12:15:01 -0600

    Added finishing touches.

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ import java.util.*;
 //This is an example of a Strategy Pattern
 //For CSC 3250
 class Course{
-    private String _cnum;
+    private String _cNum;
     private int _credits;
     public Course(){}
     public Course(String num, int cred){
-        _cnum = num;  _credits = cred;
+        _cNum = num;  _credits = cred;
     }
-    public void setNumber(String num){_cnum = num;}
+    public void setNumber(String num){_cNum = num;}
     public void setCredits(int cred){_credits = cred;}
-    public String getNumber(){return _cnum;}
+    public String getNumber(){return _cNum;}
     public int getCredits(){return _credits;}
-    public String toString(){return _cnum + " " + _credits;}
+    public String toString(){return _cNum + " " + _credits;}
 }
 class Student{
     private String _sid;
@@ -22,11 +22,12 @@ class Student{
     public String getID(){return _sid;}
     public String toString(){return _sid;}
 }
-interface SearchBehavior<T, S>{
+interface SearchBehavior<T, S>{ // Strategy Interface - Important for strategy class.
     // T is the object, and S is the value (usually primitive)
     boolean search(T obj, S v);
 }
-class StudentSearch implements SearchBehavior<Student, String>{
+class StudentSearch implements SearchBehavior<Student, String>{ //Behavior Class - Important for strategy class
+    // Limits duplicate code
     @Override
     public boolean search(Student obj, String v) { //Uses the SearchBehavior interface for class Student
         return obj.getID().equals(v);
@@ -38,19 +39,14 @@ class CourseSearch implements SearchBehavior<Course, String>{ //Uses the SearchB
         return obj.getNumber().equals(v);
     }
 }
-class AllItems<T>{ //Generic - templated class, not required for the strategy pattern.
+class AllItems<T>{ // Concrete Class - important for strategy pattern
+    // Generic - templated class, not required for the strategy pattern.
     private ArrayList<T> _items;
 
     public AllItems(){_items = new ArrayList<T>();}
+    public AllItems(int size){_items = new ArrayList<T>(size);}
     public void addItem(T t){_items.add(t);}
-    public <S> boolean isItem(S v, SearchBehavior<T, S> sb){ //sb is a polymorphic reference
-        for (T item : _items){
-            if (sb.search(item, v)){
-                return true;
-            }
-        }
-        return false;
-    }
+
     public <S> int findItem(S v, SearchBehavior<T, S> sb){
         for (int i =0; i< _items.size(); i++){
             if (sb.search(_items.get(i), v)){
@@ -70,9 +66,14 @@ class AllItems<T>{ //Generic - templated class, not required for the strategy pa
 class AllStudents{
     private AllItems<Student> _students;
     public AllStudents(){_students = new AllItems<Student>();}
+    public AllStudents(int size){_students = new AllItems<Student>(size);}
     public void addStudent(String id){_students.addItem(new Student(id));}
     public boolean isStudent(String id){
-        return _students.isItem(id, new StudentSearch());
+        if (_students.findItem(id, new StudentSearch()) != -1)
+            return true;
+        else{
+            return false;
+        }
     }
     public int findStudent(String id){
         return _students.findItem(id, new StudentSearch());
@@ -82,6 +83,15 @@ class AllStudents{
         _students.removeItem(i);
 
     }
+    public boolean modifyStudentID(String oldID, String newID){
+        int i = findStudent(oldID);
+        if (i < 0) return false;
+        else {
+            _students.getItem(i).setID(newID);
+            return true;
+        }
+    }
+    public int size(){return _students.size();}
     public String toString(){
         String s = "Students:\n";
         for (int i=0; i<_students.size(); i++)
@@ -94,19 +104,34 @@ class AllCourses{
     public AllCourses(){
         _courses = new AllItems<Course>();
     }
-    public void addCourse(String cnum, int c){
-        _courses.addItem(new Course(cnum, c));
+    public AllCourses(int size){_courses = new AllItems<Course>(size);}
+    public void addCourse(String cNum, int c){
+        _courses.addItem(new Course(cNum, c));
     }
-    public boolean isCourse(String cnum){
-        return _courses.isItem(cnum, new CourseSearch());
+    public boolean isCourse(String cNum){
+        if (_courses.findItem(cNum, new CourseSearch()) != -1){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-    public int findCourse(String cnum){
-        return _courses.findItem(cnum, new CourseSearch());
+    public int findCourse(String cNum){
+        return _courses.findItem(cNum, new CourseSearch());
     }
-    public void removeCourse(String cnum){
-        int i = _courses.findItem(cnum, new CourseSearch());
+    public void removeCourse(String cNum){
+        int i = _courses.findItem(cNum, new CourseSearch());
         _courses.removeItem(i);
     }
+    public boolean modifyCourseNumber(String oldNum, String newNum){
+        int i = findCourse(oldNum);
+        if (i < 0) return false;
+        else {
+            _courses.getItem(i).setNumber(newNum);
+            return true;
+        }
+    }
+    public int size(){return _courses.size();}
     public String toString(){
         String s = "Courses:\n";
         for (int i=0; i<_courses.size(); i++)
